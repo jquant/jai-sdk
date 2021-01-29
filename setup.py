969b751b272acc499f4fc83a772d773ec4750888
rogerioguicampos <rogerio.guicampos@gmail.com>
2021-01-29 11:45:25 -0300

    add config files to publish in PyPI

---
+++ setup.py
@@ -0,0 +1,21 @@
+"""
+"""
+import pathlib
+from setuptools import find_packages, setup
+
+
+setup(
+    name="mycelia-sdk",
+    version="0.1.0",
+    author="rogerio.guicampos",
+    author_email="rogerio.campos@jquant.com.br",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+    ],
+    packages=find_packages(exclude=("tests",)),
+    include_package_data=True,
+    install_requires=["azure-storage-blob", "brain-plasma", "numpy", "pandas"]
+)
---
+++ src/__init__.py
@@ -0,0 +1,8 @@
+"""
+--- ___init___.py ---
+
+created by @rogerioguicampos
+"""
+
+# version of mycelia-sdk package
+__version__ = "0.1.0"
