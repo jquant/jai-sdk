3d6192ceca0ad30607acec27cb775d261f1fd559
ricardokazu <43827001+ricardokazu@users.noreply.github.com>
2021-02-24 12:08:43 -0300

    Create test_jai_models.py

---
+++ jai/tests/test_jai_models.py
@@ -0,0 +1,143 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb 23 20:52:01 2021
+
+@author: Kazu
+"""
+
+from jai import Jai
+from pandas.api.types import infer_dtype
+import pandas as pd
+import numpy as np
+import pytest
+
+
+URL = 'http://localhost:8001'
+AUTH_KEY = "sdk_test"
+TITANIC_TRAIN = "https://raw.githubusercontent.com/rebeccabilbro/titanic/master/data/train.csv"
+TITANIC_TEST = "https://raw.githubusercontent.com/rebeccabilbro/titanic/master/data/test.csv"
+
+np.random.seed(42)
+
+# =============================================================================
+# Test Text
+# =============================================================================
+@pytest.mark.parametrize("name,data,dtype", [("test_nlp", "list", "Text"),
+                                        ("test_fasttext", "array", "FastText"),
+                                        ("test_edittext", "series", "TextEdit")])
+def test_text(name, data, dtype):
+    train = pd.read_csv(TITANIC_TRAIN).rename(columns={"PassengerId": "id"}).set_index("id")['Name']
+    test = pd.read_csv(TITANIC_TEST).rename(columns={"PassengerId": "id"})
+    query = test.loc[np.random.randint(0, len(test), 10)]
+
+    if data == 'list':
+        train = train.tolist()
+        test = test.set_index('id')['Name']
+    elif data == 'array':
+        train = train.values
+        test = test.set_index('id')['Name']
+    else:
+        test = test[['id', 'Name']]
+
+    j = Jai(url=URL, auth_key=AUTH_KEY)
+    j.setup(name, train, db_type=dtype, overwrite=True)
+    assert j.wait_setup(name)
+    assert j.is_valid(name), "valid name after setup failed"
+
+    ids = train.index.tolist()
+    assert j.ids(name) == [f"{len(ids)} items from {min(ids)} to {max(ids)}"], 'ids simple failed'
+    assert j.ids(name, 'complete') == ids, "ids complete failed"
+
+    result = j.similar(name, query)
+    assert isinstance(result, list), "similar result failed"
+
+    j.add_data(name, test)
+
+    ids = train.index.tolist()
+    assert j.ids(name) == [f"{len(ids)} items from {min(ids)} to {max(ids)}"], 'ids simple failed'
+    assert j.ids(name, 'complete') == ids, "ids complete failed"
+
+
+    j.delete_database(name)
+    assert not j.is_valid(name), "valid name after delete failed"
+
+
+# =============================================================================
+# Test Unsupervised
+# =============================================================================
+def test_unsupervised():
+    name = 'test_unsupervised'
+
+    train = pd.read_csv(TITANIC_TRAIN).drop(columns=["PassengerId"])
+    query = train.loc[np.random.randint(0, len(train), 10)]
+
+    j = Jai(url=URL, auth_key=AUTH_KEY)
+    j.setup(name, train, db_type="Unsupervised", overwrite=True)
+
+    assert j.wait_setup(name)
+    assert j.is_valid(name), "valid name after setup failed"
+
+    ids = train.index.tolist()
+    assert j.ids(name) == [f"{len(ids)} items from {min(ids)} to {max(ids)}"], 'ids simple failed'
+    assert j.ids(name, 'complete') == ids, "ids complete failed"
+
+    for k, v in j.fields(name).items():
+        original = infer_dtype(train[k])
+        from_api = infer_dtype([v])
+        assert original == from_api, "dtype from api {from_api} differ from data {original}"
+
+    result = j.similar(name, query)
+    assert isinstance(result, list), "similar result failed"
+
+    j.delete_database(name)
+    assert not j.is_valid(name), "valid name after delete failed"
+
+# =============================================================================
+# Test Supervised
+# =============================================================================
+def test_supervised():
+    name = 'test_supervised'
+
+    train = pd.read_csv(TITANIC_TRAIN).rename(columns={"PassengerId": "id"})
+    test = pd.read_csv(TITANIC_TEST).rename(columns={"PassengerId": "id"})
+    query = test.loc[np.random.randint(0, len(test), 10)]
+
+    j = Jai(url=URL, auth_key=AUTH_KEY)
+    j.setup(name, train, db_type="Supervised",
+            overwrite=True,
+            label={"task": "metric_classification",
+                    "label_name": "Survived"},
+            split={"type": 'stratified',
+                    "split_column": "Survived",
+                    "test_size": .2})
+
+    assert j.wait_setup(name)
+    assert j.is_valid(name), "valid name after setup failed"
+
+    ids = train['id'].tolist()
+    assert j.ids(name) == [f"{len(ids)} items from {min(ids)} to {max(ids)}"], 'ids simple failed'
+    assert j.ids(name, 'complete') == ids, "ids complete failed"
+
+    # Label returns with wrong value
+    # uncomment after solving the issue on api
+    # for k, v in j.fields(name).items():
+    #     original = infer_dtype(train[k])
+    #     from_api = infer_dtype([v])
+    #     assert original == from_api, "dtype from api {from_api} differ from data {original}"
+
+    result = j.similar(name, query)
+    assert isinstance(result, list), "similar result failed"
+
+    result = j.predict(name, query)
+    assert isinstance(result, list), "predict result failed"
+
+    j.add_data(name, test)
+
+    ids = train.index.tolist()
+    assert j.ids(name) == [f"{len(ids)} items from {min(ids)} to {max(ids)}"], 'ids simple failed'
+    assert j.ids(name, 'complete') == ids, "ids complete failed"
+
+    j.delete_database(name)
+    assert not j.is_valid(name), "valid name after delete failed"
+
+
