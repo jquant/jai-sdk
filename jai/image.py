4b49405232ad8fdbe869cf0bf46f13ea6655818b
ricardokazu <43827001+ricardokazu@users.noreply.github.com>
2021-03-11 18:19:17 -0300

    doc

--- jai/image.py
+++ jai/image.py
@@ -18,7 +18,35 @@ __all__ = ["read_image_folder"]
 def read_image_folder(image_folder: str = None,
                       images: List = None,
                       ignore_corrupt=False,
-                      extensions=[".png", ".jpg", ".jpeg"]):
+                      extensions: List = [".png", ".jpg", ".jpeg"]):
+    """
+    Function to read images from folder and transform to a format compatible to jai.
+
+    Must pass the folder of the images or a list of paths for each image.
+
+    Parameters
+    ----------
+    image_folder : str or Path, optional
+        Path for the images folder. The default is None.
+    images : List of Path, optional
+        List of Paths for each image. The default is None.
+    ignore_corrupt : TYPE, optional
+        If ignore corrupt images. If True, could probably result in a internal
+        error later on. The default is False.
+    extensions : List, optional
+        List of acceptable extensions. The default is [".png", ".jpg", ".jpeg"].
+
+    Raises
+    ------
+    ValueError
+        If not passed a image folder or list of images.
+
+    Returns
+    -------
+    pd.Series
+        Pandas Series with acceptable format for jai usage.
+
+    """
 
     if image_folder is not None:
         images = Path(image_folder).iterdir()
