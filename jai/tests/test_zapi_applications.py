1e38c05b36128135701281ad53138065e597c632
ricardokazu <43827001+ricardokazu@users.noreply.github.com>
2021-03-11 21:06:43 -0300

    commit pra identar automaticamente

--- jai/tests/test_zapi_applications.py
+++ jai/tests/test_zapi_applications.py
@@ -5,7 +5,8 @@ import pandas as pd
 import numpy as np
 import pytest
 
-URL = 'http://localhost:8001'
+# URL = 'http://localhost:8001'
+URL = 'http://23.96.99.211:8001'
 AUTH_KEY = "sdk_test"
 
 np.random.seed(42)
@@ -33,3 +34,97 @@ def test_embedding(name, setup_dataframe):
 
     j.delete_database(name)
     assert not j.is_valid(name), "valid name after delete failed"
+
+
+@pytest.mark.parametrize("name", ["test_match"])
+def test_embedding(name, setup_dataframe):
+
+    A = ['Mandarin', 'Raspberry', 'Plum', 'Coconut', 'Kiwi', 'Grapefruit',
+         'Grape', 'Lemon', 'Mandarin', 'Grape', 'Raspberry', 'Cherry', 'Plum',
+        'Apple', 'Raspberry', 'Apricot', 'Watermelon', 'Blueberry', 'Pineapple',
+        'Peach', 'Lime', 'Coconut', 'Mango', 'Grape', 'Avocado', 'Apricot',
+        'Avocado', 'Pineapple', 'Avocado', 'Apple', 'Avocado',
+     'Lemon',
+     'Lime',
+     'Cherry',
+     'Mandarin',
+     'Lime',
+     'Avocado',
+     'Papaya',
+     'Mandarin',
+     'Apple',
+     'Apple',
+     'Pear',
+     'Papaya',
+     'Papaya',
+     'Apple',
+     'Nectarine',
+     'Avocado',
+     'Apricot',
+     'Strawberry',
+     'Orange']
+
+    B = ['Blyeberry',
+     'Otsnge',
+     'Mcngo',
+     'Wqtetmelob',
+     'Jaxofruif',
+     'Lear',
+     'Leoon',
+     'Bludbecry',
+     'Kamgo',
+     'Aoricog',
+     'Zppke',
+     'Oaoaya',
+     'Appkr',
+     'Chsrrt',
+     'Lapwya',
+     'Pescj',
+     'Plym',
+     'Xnerry',
+     'Avocarp',
+     'Mqhgo',
+     'Nrctafije',
+     'Waterjepkn',
+     'Mwnearin',
+     'Apricov',
+     'Necgarinx',
+     'Grapwfeuiy',
+     'Bsnaha',
+     'Apppe',
+     'Xtrswgerry',
+     'Apold',
+     'Peqr',
+     'Nekon',
+     'Ljneaople',
+     'Hwnana',
+     'Mekoj',
+     'Oime',
+     'Lokegrahate',
+     'Aoricit',
+     'Pineapoie',
+     'Avkcaeo',
+     'Avpvado',
+     'Cuerrg',
+     'Peqr',
+     'Lsmin',
+     'Lemoj',
+     'Pomqgranatw',
+     'Aopls',
+     'Mxngi',
+     'Llmegranate',
+     'Gfapd']
+    expected = [17, 49, 22, 41,  7, 17, 22, 15, 13, 37, 13, 11, 37, 41,  2, 41,
+                24, 22,  0, 15, 45,  5, 41, 13, 13, 41,  7, 22,  7, 20, 15, 18,
+                24, 24, 11, 41,  7,  7, 13, 22,  6]
+
+    data_left = pd.Series(A)
+    data_right = pd.Series(B)
+
+    j = Jai(url=URL, auth_key=AUTH_KEY)
+    if j.is_valid(name):
+        j.delete_database(name)
+    ok = j.match(name, data_left, data_right, top_k=40, original_data=True)
+
+    assert ok['id_left'].tolist() == expected, "match failed"
+
