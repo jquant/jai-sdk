ae7b51d56270d0ebf0777ae478d607cecb010449
p-freire <pauloguilhermefreire@gmail.com>
2021-03-30 16:53:28 -0300

    new version

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open("requirements.txt", "r") as f:
 
 setup(
     name="jai-sdk",
-    version="0.6.2",
+    version="0.6.3",
     author="JQuant",
     author_email="jedis@jquant.com.br",
     description="JAI - Trust your data",
