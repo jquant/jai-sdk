5d5cfd9e2c7c44d496ac468d6da669946de09231
p-freire <pauloguilhermefreire@gmail.com>
2021-03-31 16:28:18 -0300

    and a bit more again

--- jai/tests/test_zjai_errors.py
+++ jai/tests/test_zjai_errors.py
@@ -148,3 +148,9 @@ def test_check_name_lengths_exception():
     with pytest.raises(ValueError):
         j = Jai(url=INVALID_URL, auth_key=AUTH_KEY)
         j._check_name_lengths(name="test", cols=[j.generate_name(length=35)])
+
+
+def test_check_name_lengths_exception():
+    with pytest.raises(ValueError):
+        j = Jai(url=INVALID_URL, auth_key=AUTH_KEY)
+        j._check_name_lengths(name="test", cols=[j.generate_name(length=35)])
