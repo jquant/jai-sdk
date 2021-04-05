14928b9cff9f00abf18ac119124babe123bf2315
p-freire <pauloguilhermefreire@gmail.com>
2021-04-05 16:23:46 -0300

    keys instead of items

--- jai/jai.py
+++ jai/jai.py
@@ -1431,7 +1431,7 @@ class Jai:
             # that do not satisfy the cat_threshold, but must be
             # processed anyway
             if isinstance(db_type, dict):
-                pre.extend([item for item in db_type.items() if item in cat.columns])
+                pre.extend([item for item in db_type.keys() if item in cat.columns])
             
             # we make `pre` a set to ensure it has
             # unique column names
@@ -1590,12 +1590,12 @@ class Jai:
             # that do not satisfy the cat_threshold, but must be
             # processed anyway
             if isinstance(db_type, dict):
-                pre.extend([item for item in db_type.items() if item in cat.columns])
+                pre.extend([item for item in db_type.keys() if item in cat.columns])
             
             # we make `pre` a set to ensure it has
             # unique column names
             pre = set(pre)
-            
+
             if columns_ref is None:
                 columns_ref = cat.columns.tolist()
             elif not isinstance(columns_ref, list):
