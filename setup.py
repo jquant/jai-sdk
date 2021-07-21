29522f6c3dd531635cc067ccea390fa4d1c82e12
ricardokazu <43827001+ricardokazu@users.noreply.github.com>
2021-07-20 21:05:31 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open("requirements.txt", "r") as f:
 
 setup(
     name="jai-sdk",
-    version="0.13.2",
+    version="0.14.0",
     author="JQuant",
     author_email="jedis@jquant.com.br",
     description="JAI - Trust your data",
