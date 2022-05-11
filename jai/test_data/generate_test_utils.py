9f032efed91a99fd1c70fea5e6c7cbdcd049ec49
kazu <ricardo.k.nakanishi@gmail.com>
2022-05-11 19:05:40 +0000

    .

--- jai/test_data/generate_test_utils.py
+++ jai/test_data/generate_test_utils.py
@@ -1,15 +1,24 @@
 from pathlib import Path
 
 from jai.utilities import read_image_folder
+import argparse
 
 
-def generate_read_image_folder(image_folder=Path("test_imgs")):
+def generate_read_image_folder(image_folder):
     img_data = read_image_folder(image_folder=image_folder,
                                  id_pattern="img(\d+)")
     print()
     print(img_data)
-    img_data.to_pickle(Path("test_imgs/dataframe_img.pkl"))
+    img_data.to_pickle(image_folder / "dataframe_img.pkl", protocol=4)
 
 
 if __name__ == '__main__':
-    generate_read_image_folder()
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("-p",
+                        "--path",
+                        default="test_imgs",
+                        help="Image paths.")
+    args = parser.parse_args()
+
+    generate_read_image_folder(Path(args.path))
